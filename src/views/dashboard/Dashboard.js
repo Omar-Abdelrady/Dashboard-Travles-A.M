b3a336ea2ec21e38a1123e9dc741683e4f9bba7e
Omar-AbdElrady <omarabdelrady123@gmail.com>
2021-03-28 18:24:20 +0200

    is done

---
+++ src/views/dashboard/Dashboard.js
@@ -0,0 +1,35 @@
+import React, { lazy } from 'react'
+import {
+  CBadge,
+  CButton,
+  CButtonGroup,
+  CCard,
+  CCardBody,
+  CCardFooter,
+  CCardHeader,
+  CCol,
+  CProgress,
+  CRow,
+  CCallout
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+
+import MainChartExample from '../charts/MainChartExample.js'
+import Charts from "../charts/Charts";
+
+const WidgetsDropdown = lazy(() => import('../widgets/WidgetsDropdown.js'))
+const WidgetsBrand = lazy(() => import('../widgets/WidgetsBrand.js'))
+
+const Dashboard = () => {
+  return (
+    <>
+      <WidgetsDropdown />
+
+      <WidgetsBrand withCharts/>
+
+      <Charts />
+    </>
+  )
+}
+
+export default Dashboard
